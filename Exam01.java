ab1bdb9ec6178fe29a1ea94423abb5e765ed063e
엇 뷰다 <92242267+eodeod0212@users.noreply.github.com>
2022-06-27 06:05:08 +0000

    Exam01

--- Exam01.java
+++ Exam01.java
@@ -1,5 +1,9 @@
 class Exam01 {
   public static void main(String[] args) {
-   
+    System.out.println("ㅎㅇ");
+    System.out.println("Hello");
+    System.out.print("Hello");
+    System.out.print("");
+    System.out.println("");
   }
 }
